f3a5c5d4f09bd150ed714d831f54d20d7eefab44
lmokadam <lkmokadam3@gmail.com>
2017-06-13 13:41:17 +0530

    added local system access to docker

--- install/ubuntu/run_docker.sh
+++ install/ubuntu/run_docker.sh
@@ -7,5 +7,5 @@ sudo docker build -t server .
 
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 
-sudo nohup docker run  -p 5003:5003 --name server server python /app/server.py >> server_$today.log &
-sudo nohup docker run  -p 5001:5001 --name camera server python /app/client_camera.py >> camera_$today.log &
+sudo nohup docker run  -p 5003:5003  --network="host"  --name server server python /app/server.py >> server_$today.log &
+sudo nohup docker run  -p 5001:5001  --network="host"  --name camera server python /app/client_camera.py >> camera_$today.log &
