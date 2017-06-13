b59919c5d4f14891ce0460e072c19be69ee4a45f
lmokadam <lkmokadam3@gmail.com>
2017-06-13 12:19:02 +0530

    add docker

--- install/ubuntu/run_docker.sh
+++ install/ubuntu/run_docker.sh
@@ -3,6 +3,8 @@ sudo docker rm camera
 sudo docker stop server
 sudo docker rm server
 
+sudo docker build -t server .
+
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 
 sudo nohup docker run  -p 5003:5003 --name server server python /app/server.py >> server_$today.log &
