cfe10069d11cc5c4f79ad43f7e20d8034499fa81
lmokadam <lkmokadam3@gmail.com>
2017-06-13 19:09:22 +0530

    fixed the opencv frame capture bug. After reading the frame, oldest frame was getting read from buffer. To make sure the latest frame is being read, launched a thread for grabbing the frames continuously. This ensures the latest frame read during retreieve call.

--- client_camera.py
+++ client_camera.py
@@ -1,13 +1,14 @@
-import zmq, threading, subprocess, os, tempfile,cv2
+import zmq, threading, subprocess, os, tempfile, cv2, imutils
 import time
 
 import message_protocols
 
+global camera_recording_bit
+global camera_on
 CAMERA_SERVER_PORT = "tcp://*:5001"
-recording_bit = False
+
+
 class client_camera_system():
-
-
     _context = None
     _socket = None
     _out_video_file = None
@@ -17,62 +18,66 @@ class client_camera_system():
         self._context = zmq.Context()
         self._socket = self._context.socket(zmq.REP)
         self._socket.bind(CAMERA_SERVER_PORT)
-        self._recording_bit = False
+        global camera_on
+        camera_on = True
 
+    def frame_grabber_daemon(self):
+        # self.set_camera_prop()
+        self._cap = cv2.VideoCapture(0)
+        while (camera_on):
+            self._cap.grab()
+        self._cap.release()
+
+    def set_camera_prop(self):
+        subprocess.call("v4l2-ctl -c white_balance_temperature_auto=0", shell=True)
+        subprocess.call("v4l2-ctl -c exposure_auto=0", shell=True)
 
     def restart_connection(self):
         pass
 
-    def get_message(self,expected_message):
+    def get_message(self, expected_message):
         message = self._socket.recv()
         if not expected_message == message:
-            print "error : received '", message," ' instead of '", expected_message, "'"
+            print "error : received '", message, " ' instead of '", expected_message, "'"
             self.restart_connection()
 
         print "GOT : ", message
-
 
     def send_message(self, message):
         self._socket.send(message)
         print "SENT : ", message
 
+    def start_video_record(self, folder_path, video_name):
 
+        if imutils.is_cv2():
+            fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+        else:
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        video_file_path = os.path.join(folder_path, video_name)
+        self._out_video_file = cv2.VideoWriter(video_file_path, fourcc, 20.0, (640, 480))
+        print video_file_path
+        global camera_recording_bit
+        camera_recording_bit = True
 
-    def start_video_record(self,folder_path, video_name):
-        global recording_bit
-        self._cap = cv2.VideoCapture(0)
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self._out_video_file = cv2.VideoWriter(os.path.join(folder_path,video_name), fourcc, 20.0, (640, 480))
         while (self._cap.isOpened()):
-            ret, frame = self._cap.read()
-            if recording_bit == False:
+            if not camera_recording_bit:
                 break
-            if ret == True:
+            ret, frame = self._cap.retrieve()
+            if ret:
                 self._out_video_file.write(frame)
-            else:
-                break
 
-
-
-    def stop_video_record(self,folder_path, video_name):
-        global recording_bit
-        self._recording_bit = False
+    def stop_video_record(self):
+        global camera_recording_bit
+        camera_recording_bit = False
+        time.sleep(1)
+        camera_on = False
         self._out_video_file.release()
-        self._cap.release()
-        video_avi_file_path = os.path.join(folder_path,video_name)
-        video_mp4_file_path = os.path.join(folder_path, "video.mp4")
-        conv_proc = subprocess.Popen("ffmpeg -i "+video_avi_file_path+" "+video_mp4_file_path)
-        while conv_proc.poll():
-            time.sleep(1)
 
+    def take_image(self, folder_path, img_name):
 
-    def take_image(self,folder_path, img_name):
-        cap = cv2.VideoCapture(0)
-        ret,img = cap.read()
-        cv2.imwrite(os.path.join(folder_path,img_name), img)
-        cap.release()
-
-
+        ret, img = self._cap.retrieve()
+        cv2.imwrite(os.path.join(folder_path, img_name), img)
+        time.sleep(1)
 
     def run(self):
 
@@ -80,14 +85,17 @@ class client_camera_system():
         print temp_dir
 
         self.get_message(message_protocols.CLIENT_TEST_SYSTEM_START_TESTING)
+        frame_grabber = threading.Thread(target=self.frame_grabber_daemon)
+        frame_grabber.start()
         self.send_message(message_protocols.CLIENT_CAMERA_READY_TO_CAPTURED)
 
         self.get_message(message_protocols.CLIENT_CAMERA_CAPTURE_QR)
-        self.take_image(temp_dir,"test_qr.jpg")
+        self.take_image(temp_dir, "test_qr.jpg")
+
         self.send_message(message_protocols.CLIENT_CAMERA_QR_CODE_CAPTURED)
 
         self.get_message(message_protocols.CLIENT_CAMERA_CAPTURE_BLANK_SCREEN)
-        self.take_image(temp_dir,"ref.jpg")
+        self.take_image(temp_dir, "ref.jpg")
         self.send_message(message_protocols.CLIENT_CAMERA_BLANK_SCREEN_CAPTURED)
 
         self.get_message(message_protocols.CLIENT_CAMERA_CAPTURE_VIDEO)
@@ -96,11 +104,12 @@ class client_camera_system():
         self.send_message(message_protocols.CLIENT_CAMERA_VIDEO_CAPTURED)
 
         self.get_message(message_protocols.CLIENT_CAMERA_STOP_CAPTURING_VIDEO)
-        self.stop_video_record(temp_dir, "video.avi")
+        self.stop_video_record()
         self.send_message(message_protocols.CLIENT_CAMERA_VIDEO_CAPTURE_STOPPED)
 
         self.get_message(message_protocols.CLIENT_CAMERA_DONE)
         self.send_message(message_protocols.CLIENT_CAMERA_RELEASING_RESOURCES)
+
 
 camera_thread = client_camera_system()
 while True:
--- server.py
+++ server.py
@@ -8,13 +8,13 @@ CAMERA = "CAMERA"
 
 SERVER_TCP_PORT = "tcp://*:5003"
 CAMERA_IP_PORT = "tcp://localhost:5001"
+
+
 class server:
     _context = None
     _socket_camera = None
     _socket_test_machine = None
     _socket_camera = None
-
-
 
     def restart_server(self):
         pass
@@ -27,9 +27,7 @@ class server:
         self._socket_camera = self._context.socket(zmq.REQ)
         self._socket_camera.connect(CAMERA_IP_PORT)
 
-
-
-    def get_message(self,machine,expected_message):
+    def get_message(self, machine, expected_message):
         if machine == CAMERA:
             message = self._socket_camera.recv()
         elif machine == TEST_MACHINE:
@@ -38,12 +36,11 @@ class server:
             os._exit(1)
 
         if not expected_message == message:
-            print "error : received '", message," ' instead of '", expected_message, "'"
+            print "error : received '", message, " ' instead of '", expected_message, "'"
             self.restart_server()
         print "GOT : ", message
 
-
-    def send_message(self, machine,message):
+    def send_message(self, machine, message):
         if machine == "CAMERA":
             self._socket_camera.send(message)
         elif machine == TEST_MACHINE:
@@ -52,31 +49,31 @@ class server:
             os._exit(1)
         print "SENT : ", message
 
-
-
     def run(self):
-        self.get_message(machine=TEST_MACHINE,expected_message=message_protocols.CLIENT_TEST_SYSTEM_START_TESTING)
+        self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_SYSTEM_START_TESTING)
 
-        self.send_message(machine=CAMERA,message=message_protocols.CLIENT_TEST_SYSTEM_START_TESTING)
-        self.get_message(machine=CAMERA,expected_message=message_protocols.CLIENT_CAMERA_READY_TO_CAPTURED)
+        self.send_message(machine=CAMERA, message=message_protocols.CLIENT_TEST_SYSTEM_START_TESTING)
+        self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_READY_TO_CAPTURED)
 
-        self.send_message(machine=TEST_MACHINE,message = message_protocols.CLIENT_TEST_MACHINE_LAUNCH_QR_CODE)
-        self.get_message(machine=TEST_MACHINE,expected_message=message_protocols.CLIENT_TEST_MACHINE_QR_CODE_LAUNCHED)
+        self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_LAUNCH_QR_CODE)
+        self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_QR_CODE_LAUNCHED)
 
         self.send_message(machine=CAMERA, message=message_protocols.CLIENT_CAMERA_CAPTURE_QR)
-        self.get_message(machine=CAMERA,expected_message=message_protocols.CLIENT_CAMERA_QR_CODE_CAPTURED)
+        self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_QR_CODE_CAPTURED)
 
         self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_KILL_QR_CODE)
         self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_QR_CODE_KILLED)
 
         self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_LAUNCH_BLANK_SCREEN)
-        self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_BLANK_SCREEN_LAUNCHED)
+        self.get_message(machine=TEST_MACHINE,
+                         expected_message=message_protocols.CLIENT_TEST_MACHINE_BLANK_SCREEN_LAUNCHED)
 
         self.send_message(machine=CAMERA, message=message_protocols.CLIENT_CAMERA_CAPTURE_BLANK_SCREEN)
         self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_BLANK_SCREEN_CAPTURED)
 
         self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_KILL_BLANK_SCREEN)
-        self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_BLANK_SCREEN_KILLED)
+        self.get_message(machine=TEST_MACHINE,
+                         expected_message=message_protocols.CLIENT_TEST_MACHINE_BLANK_SCREEN_KILLED)
 
         self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_LAUNCH_VIDEO)
         self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_VIDEO_LAUNCHED)
@@ -84,15 +81,15 @@ class server:
         self.send_message(machine=CAMERA, message=message_protocols.CLIENT_CAMERA_CAPTURE_VIDEO)
         self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_VIDEO_CAPTURED)
 
-
-        self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_SEND_SIGNAL_AFTER_VIDEO_ENDS)
+        self.send_message(machine=TEST_MACHINE,
+                          message=message_protocols.CLIENT_TEST_MACHINE_SEND_SIGNAL_AFTER_VIDEO_ENDS)
         self.get_message(machine=TEST_MACHINE, expected_message=message_protocols.CLIENT_TEST_MACHINE_VIDEO_KILLED)
 
         self.send_message(machine=CAMERA, message=message_protocols.CLIENT_CAMERA_STOP_CAPTURING_VIDEO)
         self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_VIDEO_CAPTURE_STOPPED)
 
-        self.send_message(machine=CAMERA,message=message_protocols.CLIENT_CAMERA_DONE)
-        self.get_message(machine=CAMERA,expected_message=message_protocols.CLIENT_CAMERA_RELEASING_RESOURCES)
+        self.send_message(machine=CAMERA, message=message_protocols.CLIENT_CAMERA_DONE)
+        self.get_message(machine=CAMERA, expected_message=message_protocols.CLIENT_CAMERA_RELEASING_RESOURCES)
 
         self.send_message(machine=TEST_MACHINE, message=message_protocols.CLIENT_TEST_MACHINE_ALL_TRANSACTION_COMPLETED)
 
