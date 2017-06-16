6fcd2e6eacc5aa1d67cf1c8c8085c8020ebce7de
lmokadam <lkmokadam3@gmail.com>
2017-06-16 16:50:19 +0530

    Updated Dockerfile

--- install/ubuntu/run_docker.sh
+++ install/ubuntu/run_docker.sh
@@ -1,10 +1,7 @@
-sudo docker stop camera
-sudo docker rm camera
-sudo docker stop server
-sudo docker rm server
+sudo docker stop camera_system
+sudo docker rm camera_system
+sudo docker rmi camera_system
 
 sudo docker build -t server .
-
-today=`date '+%Y_%m_%d__%H_%M_%S'`;
 
 sudo docker run  -p 5003:5003 -p 5001:5001 --privileged --network="host"  --name camera_system camera_system
